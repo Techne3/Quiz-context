08bc31f3cdcf8a2be5268de9c97d5de61e73fc12
Timothy Sharkey <Techne3@gmail.com>
2019-11-28 08:31:09 -0700

    Wrote function to test if answers are correct Result component now shows results of quiz

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -8,9 +8,7 @@ import Answers from './components/Answers';
 
 function App() {
 
-  const [currentQuestion, setCurrentQuestion]=useState(0)
-  const [currentAnswer, setCurrentAnswer]=useState('')
-
+ 
   const questions = [
     {
         id: 1,
@@ -44,23 +42,115 @@ function App() {
 ];
 
 
+
+const [currentQuestion, setCurrentQuestion]=useState(0)
+const [currentAnswer, setCurrentAnswer]=useState('')
+const [showResults, setShowResults] = useState(false)
+const [answers,setAnswers] = useState([]);
+const [error, setError] = useState('')
+
+
 const question = questions[currentQuestion];
 
 const handleClick = e => {
   setCurrentAnswer(e.target.value)
+  //once  user picks an option clear the error message
+  setError('')
+};
 
+// set an error if there is no question picked
+  const renderError = e => {
+    if(!error){
+      return
+    }
+      return<div className="error">{error}</div>
+  }
+
+
+
+
+
+//show the question on the result component
+const renderResultsData = ()=> {
+  return answers.map(answered => {
+    const questioned = questions.find(question => question.id === answered.questionId)
+    
+    return(
+        <div key={questioned.id}>
+           {questioned.question} - {renderResultsMark(questioned, answered)}
+        </div>
+    )
+  })
 }
+
+  // show the result of quiz
+  const renderResultsMark = (questioned, answered) => {
+    if(questioned.correct_answer === answered.answer){
+      return <span className="correct">Correct</span>
+    }
+      return <span className="failed">Failed</span>
+  }
+
+
+
+//restart quiz
+const restart = ()=> {
+  setAnswers([]);
+  setCurrentAnswer('')
+  setCurrentQuestion(0)
+  setShowResults(false)
+}
+
+
+
+
+const next = e => {
+  const answer = {questionId: question.id, answer: currentAnswer}
+  //if they don't select an option set Error
+    if(!currentAnswer){
+      setError('Please select an option')
+      return;
+    }
+
+    answers.push(answer)
+    setAnswers(answers)
+    setCurrentAnswer('')
+
+    
+  if(currentQuestion + 1 < questions.length){
+    setCurrentQuestion(currentQuestion + 1)
+    return 
+  }
+   setShowResults(true);
+}
+
+ 
+
+// Show the results or else show quiz
+if(showResults){
+  return (
+    <div className="container results">
+      <h2>Results</h2>
+         <ul> {renderResultsData()}  </ul>
+      <button className='btn btn-primary' onClick={restart}>
+        Restart
+      </button>
+    </div>
+  )
+}else{
 
   return (
     <div className="container">
-      <Progress  total='3' current='1'/>
-      <Question question={question.questions} />
+      <Progress  total={questions.length} current={currentQuestion + 1}/>
+      <Question question={question.question} />
+        {renderError()}
       <Answers question={question}  currentAnswer={currentAnswer}  handleClick={handleClick}/>
-      <button className='btn btn-primary'>
+      <button className='btn btn-primary' onClick={next}>
         Confirm and Continue
       </button>
     </div>
   );
+  }
 }
 
 export default App;
--- quiz/src/components/Answer.js
+++ quiz/src/components/Answer.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Answer = (props) => {
 
+    //setting the selected answer to a new className ".answer.selected"
     let classes = ['answer']
-
     if(props.selected){
         classes.push('selected')
     }
+
+
     return (
         <div>
             <button value={props.letter}  className={classes.join(' ')}  onClick={props.handleClick}>
